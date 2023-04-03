2610429f1f80c55ce12cfe3a829c7667d727db50
vanshika <user@DESKTOP-LSEDR8U>
2023-04-03 22:11:53 +0530

    second commit

--- OnlineExam.DataAccess/Data/ApplicationDbContext.cs
+++ OnlineExam.DataAccess/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineExam.Models;
 
 namespace OnlineExamination.Data
 {
@@ -9,5 +10,6 @@ namespace OnlineExamination.Data
         {
 
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
